e93a9b207ff52708552022efeeca04938218fc30
komachan <tkkm02@gmail.com>
2025-05-10 15:29:15 +0900

    勤怠登録画面のUI修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -24,12 +24,13 @@
                     <li><a href="{{ route('admin.application.list') }}">申請一覧</a></li>
                 @else
                     <li><a href="{{ route('attendance.index') }}">勤怠</a></li>
+                    <li><a href="{{ route('attendance.list') }}">勤怠一覧</a></li>
                     <li><a href="{{ route('application.list') }}">申請一覧</a></li>
                 @endif
                 <li>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <button type="submit" class="logout-link">ログアウト</button>
+                        <button type="submit" class="logout-link nav-link-button">ログアウト</button>
                     </form>
                 </li>
             </ul>
@@ -37,19 +38,19 @@
         @endauth
     </header>
 
+    @if (session('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
+
+    @if (session('error'))
+        <div class="alert alert-error">
+            {{ session('error') }}
+        </div>
+    @endif
+
     <main class="main-content container">
-        @if (session('message'))
-            <div class="alert alert-success">
-                {{ session('message') }}
-            </div>
-        @endif
-
-        @if (session('error'))
-            <div class="alert alert-error">
-                {{ session('error') }}
-            </div>
-        @endif
-
         @yield('content')
     </main>
 </body>
