9b5ab8cc1b133d74b344d68203c26431c5103f3c
komachan <tkkm02@gmail.com>
2025-05-10 12:16:34 +0900

    管理者ログイン画面を通常ログインと統一したCSSスタイルに修正

--- src/resources/views/admin/auth/login.blade.php
+++ src/resources/views/admin/auth/login.blade.php
@@ -5,34 +5,30 @@
 @endsection
 
 @section('content')
-<div class="container login-form-wrapper">
+<div class="form-container">
     <h2 class="form-title">管理者ログイン</h2>
 
-    @if ($errors->any())
-        <div class="alert-error">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form method="POST" action="{{ route('admin.login.submit') }}" class="login-form-box">
+    <form action="{{ route('admin.login') }}" method="POST" class="form-box">
         @csrf
 
         <div class="form-group">
-            <label for="email" class="form-label">メールアドレス</label>
-            <input type="email" name="email" id="email" class="form-input" value="{{ old('email') }}" required>
+            <label for="email">メールアドレス</label>
+            <input type="email" name="email" class="form-input" value="{{ old('email') }}">
+            @error('email')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="form-group">
-            <label for="password" class="form-label">パスワード</label>
-            <input type="password" name="password" id="password" class="form-input" required>
+            <label for="password">パスワード</label>
+            <input type="password" name="password" class="form-input">
+            @error('password')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
-        <div class="form-group text-center">
-            <button type="submit" class="btn btn-login">ログイン</button>
+        <div class="form-group center">
+            <button type="submit" class="submit-button">管理者ログインする</button>
         </div>
     </form>
 </div>
