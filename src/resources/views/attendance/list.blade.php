a04fcf24ce1baee4df09064b3c74977dfb21d554
komachan <tkkm02@gmail.com>
2025-05-11 18:04:04 +0900

    レスポンシブ対応とスクロールバグ修正（style.css 他）

--- src/resources/views/attendance/list.blade.php
+++ src/resources/views/attendance/list.blade.php
@@ -4,6 +4,8 @@
 <link rel="stylesheet" href="{{ asset('css/user/attendance-list.css') }}">
 @endsection
 
+@section('main-class', 'scrollable-page')
+
 @section('content')
 <div class="attendance-container">
     <h2 class="title">勤怠一覧</h2>
